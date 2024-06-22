562cd5a0f067a93377aee4bef38f3d0565cd3414
nallruf <nalzaibaidin@gmail.com>
2024-06-22 18:19:09 +0700

    update app.js

--- app.js
+++ app.js
@@ -53,11 +53,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 
 module.exports = app;
