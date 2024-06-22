bce203d6daa9230fbbee7d38cb62c8302cea085d
nallruf <nalzaibaidin@gmail.com>
2024-06-22 17:51:52 +0700

    fix

--- app.js
+++ app.js
@@ -53,9 +53,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// const port = 4000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
-
 module.exports = app;
